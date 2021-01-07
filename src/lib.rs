c88ef94c07e688da5a50374f84a9939d444e0e7d
suren <suren-m>
2021-01-07 17:02:33 +0000

    updated comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,25 @@
 //! rsw-lib provides libraries needed for rsw-app.
-/// # User Factory
-/// User factory provides a way to create `n` number of users
-/// Example:
-/// ```rust
-///        use rsw_lib::device::*;
-///        use rsw_lib::events::*;
-///        use rsw_lib::user::UserFactory;
-///
-///        let capacity = 10;
-///        let request = 5;
-///        let factory = UserFactory::new(capacity)
-///            .create_users(request)
-///            .unwrap();
-///        let res = factory.get_users();
-///        assert_eq!(request, res.len());
-///
-///        let additional = 5;
-///        let factory = factory.create_users(additional).unwrap();
-///        let res = factory.get_users();
-///        assert_eq!(10, res.len());
-/// ```
+//! # User Factory
+//! User factory provides a way to create `n` number of users
+//! Example:
+//! ```rust
+//!        use rsw_lib::device::*;
+//!        use rsw_lib::events::*;
+//!        use rsw_lib::user::UserFactory;
+//!
+//!        let capacity = 10;
+//!        let request = 5;
+//!        let factory = UserFactory::new(capacity)
+//!            .create_users(request)
+//!            .unwrap();
+//!        let res = factory.get_users();
+//!        assert_eq!(request, res.len());
+//!
+//!        let additional = 5;
+//!        let factory = factory.create_users(additional).unwrap();
+//!        let res = factory.get_users();
+//!        assert_eq!(10, res.len());
+//! ```
 pub mod device;
 pub mod events;
 pub mod user;
